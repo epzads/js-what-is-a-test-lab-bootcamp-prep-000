18e449907db982929a3549e91e579e31413e39a3
favorable-vector-1743 <epzads@gmail.com>
2019-02-12 16:56:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = 34
 var message = ` ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-  
+
 function sayhelloTo(firstName){
     console.log("Hello"(firstName))
 }
